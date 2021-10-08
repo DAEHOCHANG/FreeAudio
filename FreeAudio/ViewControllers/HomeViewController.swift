82d6f86cf8dbe49539a873ea091bdedc7bfbfefc
DaeHoChang <eogh1209@gmail.com>
2021-10-08 17:05:30 +0900

    [Change] HomeView stopped because need to AudioManager's new features

--- FreeAudio/ViewControllers/HomeViewController.swift
+++ FreeAudio/ViewControllers/HomeViewController.swift
@@ -9,15 +9,95 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var ringTunesTableView: UITableView!
+    @IBOutlet weak var ringTunesTableViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var finderTableView: UITableView!
+    @IBOutlet weak var finderTableViewHeightConstraint: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableSetting()
+        tablesHeightSetting()
+    }
+    
+    func tablesHeightSetting() {
+        ringTunesTableViewHeightConstraint.constant = 280
+        finderTableViewHeightConstraint.constant = 500
+    }
+    func tableSetting() {
+        ringTunesTableView.delegate = self
+        ringTunesTableView.dataSource = self
         
+        finderTableView.delegate = self
+        finderTableView.dataSource = self
         
+        finderTableView.rowHeight = UITableView.automaticDimension
+        ringTunesTableView.rowHeight = UITableView.automaticDimension
+        
+        ringTunesTableView.isScrollEnabled = false
+        finderTableView.isScrollEnabled = false
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-
-    }
 }
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let number:Int = {
+            if tableView == ringTunesTableView {
+                return 1
+            } else {
+                return 10
+            }
+        }()
+        return number
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let text: String = {
+            if tableView == ringTunesTableView {
+                return "벨소리"
+            } else {
+                return "나의 아이폰"
+            }
+        }()
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = text
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel?.text = text
+        }
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UITableViewHeaderFooterView()
+        let font = UIFont.systemFont(ofSize: 25)
+        let text: String = {
+            if tableView == ringTunesTableView {
+                return "벨소리"
+            } else {
+                return "나의 아이폰"
+            }
+        }()
+        
+        if #available(iOS 14.0, *) {
+            var content = header.defaultContentConfiguration()
+            content.text = text
+            content.textProperties.font = font
+            header.contentConfiguration = content
+        } else {
+            header.textLabel?.text = text
+            header.textLabel?.font = font
+        }
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        50
+    }
+    
+    
+}
