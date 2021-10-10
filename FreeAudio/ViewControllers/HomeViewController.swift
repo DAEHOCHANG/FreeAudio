cc81b95b61ae73d83f4654f3e33ba9ec91b364dd
DaeHoChang <eogh1209@gmail.com>
2021-10-11 00:55:58 +0900

    [Create] NavigationBarbutton items , som ActionFunction( current empty)

--- FreeAudio/ViewControllers/HomeViewController.swift
+++ FreeAudio/ViewControllers/HomeViewController.swift
@@ -14,13 +14,21 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var finderTableView: UITableView!
     @IBOutlet weak var finderTableViewHeightConstraint: NSLayoutConstraint!
     var curDir = readMyIphoneDirectorysInfos()
+    var prevBarItems:[UIBarButtonItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableSetting()
         tablesHeightSetting()
+        barButtonItemSetting()
     }
-    
+    func barButtonItemSetting() {
+        let prevBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.right"), style: .plain, target: self, action: #selector(prevButtonAction(sender:)))
+        let deleteBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "trash"), style: .plain, target: self, action: #selector(deleteButtonAction(sender:)))
+        let shareBarButtonItem =  UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(shareButtonAction(sender:)))
+        let exportRingtoneBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "alarm"), style: .plain, target: self, action: #selector(exportRingtoneButtonAction(sender:)))
+        prevBarItems = [prevBarButtonItem,exportRingtoneBarButtonItem,shareBarButtonItem,deleteBarButtonItem]
+    }
     func tablesHeightSetting() {
         let numberOfFilesInCurDir = curDir.count
         
@@ -44,7 +52,42 @@ class HomeViewController: UIViewController {
 
 }
 
+/*NavigationBar item button action
+ 편집 누를시 다른 바 버튼들을 넣어줘야 하고 -> 버튼(혹은 슬라이드가 될련지) 을 통해 원상태로 돌아와야함
+ 그러한 이유로 다음과 같은 생각을 했음
+ 1. 클래스 자체에 두종류의 rigthBarButtonItems를 저장해두었다가 교체해주는 방식
+ 2. 이전 rigthBarButtonItems 만을 저장해 두었다가 "편집"버튼과 "->" 버튼을 눌러 교체해가는 방식
+*/
+extension HomeViewController {
+    @IBAction func editButtonAction(sender:UIBarButtonItem) {
+        changeBarButtonItems()
+    }
+    @objc func prevButtonAction(sender:UIBarButtonItem) {
+        changeBarButtonItems()
+    }
+    @objc func deleteButtonAction(sender:UIBarButtonItem) {
+        print("delete")
+    }
+    @objc func shareButtonAction(sender:UIBarButtonItem) {
+        print("shareButton")
+    }
+    @objc func exportRingtoneButtonAction(sender:UIBarButtonItem) {
+        print("exportRingtone")
+    }
+    
+    func changeBarButtonItems() {
+        guard let tmp = self.navigationController?.navigationBar.items?[0].rightBarButtonItems else {return}
+        //self.navigationController?.navigationItem.setRightBarButtonItems(prevBarItems, animated: true)
+        self.navigationController?.navigationBar.topItem?.setRightBarButtonItems(prevBarItems, animated: true)
+        //self.navigationController?.navigationBar.items?[0].rightBarButtonItems = prevBarItems
+        prevBarItems = tmp
+    }
+}
+
+
+//tableView Delegate, Datasource
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let number:Int = {
             if tableView == ringTunesTableView {
