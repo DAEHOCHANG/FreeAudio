fd8fe5fe9047d59e3e33850825ec81b992f00d00
DaeHoChang <eogh1209@gmail.com>
2021-10-11 15:30:39 +0900

    [Create] buttons action, multipleSelectMode,  HomeViewRingtoneDetailInfoViewController's  audioFileName var, audioAvasset var

--- FreeAudio/ViewControllers/HomeViewController.swift
+++ FreeAudio/ViewControllers/HomeViewController.swift
@@ -15,6 +15,8 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var finderTableViewHeightConstraint: NSLayoutConstraint!
     var curDir = readMyIphoneDirectorysInfos()
     var prevBarItems:[UIBarButtonItem] = []
+    var multipleSelectMode:Bool = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +29,7 @@ class HomeViewController: UIViewController {
         let deleteBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "trash"), style: .plain, target: self, action: #selector(deleteButtonAction(sender:)))
         let shareBarButtonItem =  UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(shareButtonAction(sender:)))
         let exportRingtoneBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "alarm"), style: .plain, target: self, action: #selector(exportRingtoneButtonAction(sender:)))
-        prevBarItems = [prevBarButtonItem,exportRingtoneBarButtonItem,shareBarButtonItem,deleteBarButtonItem]
+        prevBarItems = [prevBarButtonItem,exportRingtoneBarButtonItem,deleteBarButtonItem,shareBarButtonItem]
     }
     func tablesHeightSetting() {
         let numberOfFilesInCurDir = curDir.count
@@ -49,7 +51,7 @@ class HomeViewController: UIViewController {
         ringTunesTableView.isScrollEnabled = false
         finderTableView.isScrollEnabled = false
     }
-
+    
 }
 
 /*NavigationBar item button action
@@ -57,35 +59,53 @@ class HomeViewController: UIViewController {
  그러한 이유로 다음과 같은 생각을 했음
  1. 클래스 자체에 두종류의 rigthBarButtonItems를 저장해두었다가 교체해주는 방식
  2. 이전 rigthBarButtonItems 만을 저장해 두었다가 "편집"버튼과 "->" 버튼을 눌러 교체해가는 방식
+ 2번 선택 완료
 */
 extension HomeViewController {
     @IBAction func editButtonAction(sender:UIBarButtonItem) {
         changeBarButtonItems()
+        finderTableView.allowsMultipleSelection = true
+        multipleSelectMode = true
+        
     }
     @objc func prevButtonAction(sender:UIBarButtonItem) {
         changeBarButtonItems()
+        finderTableView.allowsMultipleSelection = false
+        multipleSelectMode = false
+        
     }
     @objc func deleteButtonAction(sender:UIBarButtonItem) {
-        print("delete")
+        print(finderTableView.indexPathsForSelectedRows)
+        let alert = UIAlertController(title: "정말 삭제하시겠습니까?", message: "한번 삭제하면 되돌리지 못합니다.", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let delete = UIAlertAction(title: "삭제", style: .destructive, handler: nil)
+        alert.addAction(cancel)
+        alert.addAction(delete)
+        present(alert, animated: true, completion: nil)
     }
     @objc func shareButtonAction(sender:UIBarButtonItem) {
-        print("shareButton")
+        
     }
     @objc func exportRingtoneButtonAction(sender:UIBarButtonItem) {
-        print("exportRingtone")
+        let alert = UIAlertController(title: "준비중인 기능입니다.", message: "감사합니다..", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let delete = UIAlertAction(title: "확인", style: .destructive, handler: nil)
+        alert.addAction(cancel)
+        alert.addAction(delete)
+        present(alert, animated: true, completion: nil)
     }
     
     func changeBarButtonItems() {
         guard let tmp = self.navigationController?.navigationBar.items?[0].rightBarButtonItems else {return}
-        //self.navigationController?.navigationItem.setRightBarButtonItems(prevBarItems, animated: true)
         self.navigationController?.navigationBar.topItem?.setRightBarButtonItems(prevBarItems, animated: true)
-        //self.navigationController?.navigationBar.items?[0].rightBarButtonItems = prevBarItems
         prevBarItems = tmp
     }
 }
 
 
-//tableView Delegate, Datasource
+
+
+//tableView Delegate, Datasource 테이블뷰 와 관련된것들
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -151,11 +171,26 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return 50
     }
     
-    //셀 클릭하는 순간 모달뷰가 나와야함
+    /*
+     모드에 따라 다른 액션취해야함
+     1. 셀 클릭하는 순간 모달뷰가 나와야함
+     2. 여러개를 선택 할 수 있으며 드래그도 가능
+        2.1 드래그를 멈추고 다시 드래그시 추가되는 형태
+     2.2 드래그를 시작한 셀의 상태에 따라 드래그가 다르게. 선택안된거면 드래그시 선택만, 이미 선택된거라면 드래그시 선택 해제만
+     2.2 1,2,3,4,5 중 2,3,4 가 선택되어있고 1에서 드래그를 할 시 1만 추가 되게끔(무조건 토글이 아님)
+     */
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: false)
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewRingtoneDetailInfoViewController")
-        vc?.modalPresentationStyle = .pageSheet
-        present(vc!, animated: true, completion: {})
+        if multipleSelectMode == false {
+            tableView.deselectRow(at: indexPath, animated: false)
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewRingtoneDetailInfoViewController") as? HomeViewRingtoneDetailInfoViewController
+            vc?.modalPresentationStyle = .pageSheet
+            vc?.audioFileName = curDir[indexPath.row]
+            vc?.audioAvasset = readAudioFile(with: curDir[indexPath.row])
+            present(vc!, animated: true, completion: {})
+        } else {
+            
+        }
     }
+    
+
 }
--- FreeAudio/ViewControllers/HomeViewRingtoneDetailInfoViewController.swift
+++ FreeAudio/ViewControllers/HomeViewRingtoneDetailInfoViewController.swift
@@ -7,15 +7,33 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class HomeViewRingtoneDetailInfoViewController : UIViewController {
     
     @IBOutlet weak var modalView: UIView!
+    var audioFileName:String = ""
+    var audioAvasset:AVAsset?
     override func viewDidLoad() {
         super.viewDidLoad()
         let bounds = UIScreen.main.bounds
         let deviceWidth = bounds.width
         modalView.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
         modalView.layer.cornerRadius = deviceWidth / 9.71209
+    }
+    @IBAction func shareButtonAction(_ sender: Any) {
+        
+    }
+    @IBAction func deleteButtonAction(_ sender: Any) {
+        let alert = UIAlertController(title: "정말 삭제하시겠습니까?", message: "한번 삭제하면 되돌리지 못합니다.", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let delete = UIAlertAction(title: "삭제", style: .destructive, handler: nil)
+        alert.addAction(cancel)
+        alert.addAction(delete)
+        present(alert, animated: true, completion: nil)
+    }
+    @IBAction func makeRingtoneButtonAction(_ sender: Any) {
+    }
+    @IBAction func editButtonAction(_ sender: Any) {
     }
 }
